a2ce72cb354524389f0b389c36932a05a16233d8
gregwinther <sebastwi@uio.no>
2017-07-12 18:39:14 +0200

    Started implemeting bubblesort

--- airlines.py
+++ airlines.py
@@ -9,6 +9,7 @@ class Airline:
     def __init__(self, capacity=0, price=0):
         self.capacity = capacity
         self.price = price
+        self.seats_left = capacity
 
     def set_capacity(self, new_capacity):
         """Sets capacity"""
@@ -25,6 +26,9 @@ class Airline:
     def get_price(self):
         """Returns price"""
         return self.price
+
+    def empty_airplane(self):
+        self.seats_left = self.capacity
 
 class DemandFunction:
     """Class that generates a demand function"""
@@ -47,7 +51,7 @@ class DemandFunction:
 
     def plot_demand(self):
         """Cummulates demand function and plots"""
-   
+
         # Preparing
         max_price = int(np.max(self.demand))
         min_price = int(np.min(self.demand))
@@ -67,14 +71,57 @@ class DemandFunction:
         plt.ylabel("P")
         plt.title("Demand")
         plt.show()
-    
+
+class AirlineMarket:
+    """Class that is supposed to match consumers with airlines"""
+
+    def __init__(self):
+        self.airlines = []
+        self.demand = np.array([])
+        self.no_of_airlines = 0
+
+    def add_airline(self, airline):
+        """Adds airline to the market"""
+        self.airlines.append(airline)
+        self.no_of_airlines += 1
+
+    def set_demand(self, demand):
+        """Sets demand function"""
+        self.demand = demand
+
+    def allocate_tickets(self):
+        """Market clearing function"""
+
+        for airline in self.airlines:
+            print(airline.get_price())
+
+        N = self.no_of_airlines 
+        # Sorting prices by bubble sort
+        for i in range(N - 1):
+            if self.airlines[i].get_price() > self.airlines[i + 1].get_price():
+                temp1 = self.airlines[i]
+                temp2 = self.airlines[i + 1]
+                self.airlines[i] = temp1
+                self.airlines[i + 1] = temp2
+
+
+        for airline in self.airlines:
+            print(airline.get_price())
+
 
 if __name__ == "__main__":
 
     # Test
 
     BA = Airline()
+    IB = Airline()
 
-    BA.set_capacity(100)
+    BA.set_price(20)
+    IB.set_price(30)
 
-    print(BA.get_capacity())
+    bertrand_market = AirlineMarket()
+
+    bertrand_market.add_airline(IB)
+    bertrand_market.add_airline(BA)
+
+    bertrand_market.allocate_tickets()
