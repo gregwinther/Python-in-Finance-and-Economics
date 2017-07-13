78ad7c2361abf23332c897da46f46f4f61738668
gregwinther <sebastwi@uio.no>
2017-07-13 14:46:43 +0200

    Done with object oriented structure

--- airlines.py
+++ airlines.py
@@ -6,7 +6,8 @@ from matplotlib import pyplot as plt
 class Airline:
     """Class for Airline. Getter and setter for capacity and price"""
 
-    def __init__(self, capacity=0, price=0):
+    def __init__(self, name="airline", capacity=0, price=0):
+        self.name = name
         self.capacity = capacity
         self.price = price
         self.seats_left = capacity
@@ -14,6 +15,7 @@ class Airline:
     def set_capacity(self, new_capacity):
         """Sets capacity"""
         self.capacity = new_capacity
+        self.seats_left = new_capacity
 
     def set_price(self, new_price):
         """Sets price"""
@@ -27,15 +29,28 @@ class Airline:
         """Returns price"""
         return self.price
 
+    def get_seats_left(self):
+        """Returns remaining seats"""
+        return self.seats_left
+
+    def sell_ticket(self):
+        """Reduces number of availables seats by 1"""
+        self.seats_left -= 1
+
     def empty_airplane(self):
+        """Empties plane: the number of seats left is set equal to capacity"""
         self.seats_left = self.capacity
+    
+    def get_name(self):
+        """Returns name of airline"""
+        return self.name
 
 class DemandFunction:
     """Class that generates a demand function"""
 
     def __init__(self, N=100):
         self.no_consumers = N
-        self.demand = np.ones(N) * 100
+        self.demand = np.array([])
 
     def generate_uniform(self, min_price, max_price):
         """Generates uniformly distributed demand function"""
@@ -48,6 +63,10 @@ class DemandFunction:
     def get_demand(self):
         """Returns demand function"""
         return self.demand
+
+    def set_demand(self, new_demand):
+        """Sets demand"""
+        self.demand = new_demand
 
     def plot_demand(self):
         """Cummulates demand function and plots"""
@@ -77,7 +96,7 @@ class AirlineMarket:
 
     def __init__(self):
         self.airlines = []
-        self.demand = np.array([])
+        self.demand = DemandFunction()
         self.no_of_airlines = 0
 
     def add_airline(self, airline):
@@ -88,16 +107,24 @@ class AirlineMarket:
     def set_demand(self, demand):
         """Sets demand function"""
         self.demand = demand
+    
+    def get_demand(self):
+        """Returns demand object"""
+        return self.demand
 
     def allocate_tickets(self):
         """Market clearing function"""
+
+        # Resetting airplanes
+        for airline in self.airlines:
+            airline.empty_airplane()
 
         '''
         for airline in self.airlines:
             print(airline.get_price())
         '''
-     
-        # Sorting prices by bubble sort
+
+        # Sorting airline prices by bubble sort
         swapped = True
         while swapped:
             swapped = False
@@ -108,24 +135,48 @@ class AirlineMarket:
                     self.airlines[i] = temp2
                     self.airlines[i + 1] = temp1
                     swapped = True
-        '''
+
+        # Tickets are sold
+        temp_demand = np.sort(self.demand.get_demand())
+
         for airline in self.airlines:
-            print(airline.get_price())
-        '''
+            i = 0
+            for consumer_price in temp_demand:
+
+                if airline.get_seats_left() > 0:
+
+                    if airline.get_price() < consumer_price:
+                        airline.sell_ticket()
+                        i += 1
+
+            temp_demand = temp_demand[i: -1]
+
+        print("--------------------")
+        for airline in self.airlines:
+            print("Airline: ", airline.get_name(),\
+            "Seats left: ", airline.get_seats_left(), " Price: ", airline.get_price())
+        print("--------------------")
 
 if __name__ == "__main__":
-
-    # Test
 
     BA = Airline()
     IB = Airline()
     DY = Airline()
 
     DY.set_price(10)
-    BA.set_price(20)
-    IB.set_price(30)
+    BA.set_price(25)
+    IB.set_price(40)
+
+    DY.set_capacity(50)
+    BA.set_capacity(50)
+    IB.set_capacity(50)
 
     bertrand_market = AirlineMarket()
+
+    uniform_demand = DemandFunction()
+    uniform_demand.generate_uniform(10,50)
+
+    bertrand_market.set_demand(uniform_demand)
 
     bertrand_market.add_airline(IB)
     bertrand_market.add_airline(BA)
